f4bffb716792d73d6618e5d6ea243faf875d7181
anmin <anmin0517@gmail.com>
2023-07-10 17:31:09 +0900

    0710 ssafy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
-import MyHeader from './MyHeader';
-import Counter from './Counter';
-import Container  from './Container';
+import DiaryEditor from "./DiaryEditor"
 
 function App() {
-
-
-  const counterProps = {
-    a:1,
-    b:2,
-    initialValue : 5,
-  }
-
   return (
     <div className="App">
-      <Container>
-        <MyHeader />
-        <Counter {...counterProps}/>
-      </Container>
+    <DiaryEditor/>
     </div>
   );
 }
+
 export default App;
--- src/Container.js
+++
@@ -1,9 +0,0 @@
-const Container = ({children}) => {
-    return (
-        <div style = {{margin:20, padding: 20, border: "1px solid black"}}>
-            {children}
-        </div>
-    )
-}
-
-export default Container
